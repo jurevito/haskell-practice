cd1b7a463a37d1b81a7e272a3f1e7a2d28a8d60a
Jure Vito Srovin <jure.vito@gmail.com>
2021-07-20 13:25:13 +0200

    Implement hanoi towers

--- homework/1_homework.hs
+++ homework/1_homework.hs
@@ -1,7 +1,7 @@
 -- Testing credit card number validity
 
 toDigits :: Integer -> [Integer]
-toDigits n = reverse $ toDigitsRev n
+toDigits = reverse . toDigitsRev
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n
@@ -21,25 +21,7 @@ validate xs = (sumDigits $ doubleEveryOther $ toDigits xs) `mod` 10 == 0
 
 type Peg = String
 type Move = (Peg, Peg)
+
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi 1 p1 p2 p3 = [(p1,p2)] 
---hanoi n p1 p2 p3 = ((p1,p3) : hanoi (n-1) p1 p3 p2) ++ 
-
--- 3 [a,c] + (2 [a,b]) +
--- ([1,2,3],[],[]) (a,b) 
--- ([2,3],[1],[]) (a,c)
--- ([3],[1],[2]) (b,c)
--- ([3],[],[1,2]) (a,b) -----------------
--- ([],[3],[1,2]) (c,a) -----------------
--- ([1],[3],[2]) (c,b)
--- ([1],[2,3],[]) (a,b)
--- ([],[1,2,3],[])
-
--- ([1,2,3,4],[],[]) (a,c)
--- ([2,3,4],[],[1]) (a,b)
--- ([3,4],[2],[1]) (c,b)
--- ([3,4],[1,2],[]) (a,c)
--- ([4],[1,2],[3]) (b,a)
--- ([1,4],[2],[3]) (b,c)
--- ([1,4],[],[2,3]) (a,c)
--- ([4],[],[1,2,3]) -----------------
+hanoi 0 _ _ _ = []
+hanoi n p1 p2 t1 = (hanoi (n-1) t1 p2 p1) ++ (p1,p2) : (hanoi (n-1) p1 t1 p2)
