d89edb8b743c0f10d4ca367fd90f82577de25271
Jure Vito Srovin <jure.vito@gmail.com>
2021-08-01 22:13:39 +0200

    Add whatWentWrong for homework 2

--- homework/2_homework.hs
+++ homework/2_homework.hs
@@ -13,3 +13,35 @@ parseMessage (x:xs)
 -- testParse parse 10 "Homework/SupportFiles/error.log"  
 parse :: String -> [LogMessage]
 parse xs = parseMessage <$> lines xs
+
+singleton :: LogMessage -> MessageTree
+singleton m = Node Leaf m Leaf
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert _ tree@(Node _ (Unknown _) _) = tree
+
+insert m Leaf = singleton m
+insert m1@(LogMessage _ t1 _) (Node leftTree m2@(LogMessage _ t2 _) rightTree)
+    | t1 > t2   = Node leftTree m2 (insert m1 rightTree)
+    | otherwise = Node (insert m1 leftTree) m2 rightTree
+
+-- build $ parseMessage <$> ["I 6 Completed armadillo processing", "E 65 8 Bad pickle-flange interaction detected", "I 1 Nothing to report", "I 4 Everything normal", "E 70 3 Way too many pickles", "E 20 2 Too many pickles"]
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node leftTree m rightTree) = inOrder leftTree ++ m : inOrder rightTree
+
+seriousError :: LogMessage -> Bool
+seriousError (LogMessage (Error s) _ _) = s > 50
+seriousError _ = False
+
+message :: LogMessage -> String
+message (Unknown m) = m
+message (LogMessage _ _ m) = m
+
+-- testWhatWentWrong parse whatWentWrong "Homework/SupportFiles/sample.log"
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong xs = message <$> inOrder (build $ filter seriousError xs)
