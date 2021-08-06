b43d0e2ae1a332b17a46a6c866a63cda1628e441
Jure Vito Srovin <jure.vito@gmail.com>
2021-08-06 18:28:34 +0200

    Add localMaxima to homework 3

--- homework/3_homework.hs
+++ homework/3_homework.hs
@@ -4,3 +4,10 @@ everyNth n xs = snd <$> filter (\(i, v) -> i `mod` n == 0) (zip [1..] xs)
 
 skips :: [a] -> [[a]]
 skips xs = foldr (\(n, v) acc -> everyNth n v : acc) [] (zip [1..(length xs)] (repeat xs))
+
+-- Exercise 2 Local maxima
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:xs)
+    | x < y && y > z = y : localMaxima (y:z:xs)
+    | otherwise      = localMaxima (y:z:xs)
+localMaxima _ = []
